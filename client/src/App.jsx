81b7dee2dacad19c8dc52704ad95ae3a92f2ee30
Charlize Mikaela Nadela <105686280+charlysss-l@users.noreply.github.com>
2026-05-15 10:35:49 +0000

    breadcrumbs and cards

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import Header from './components/Header'
 import Error404 from './components/Error404'
 import Pagination from "./components/Paginaton"
 import Tabs from "./components/Tabs"
+import Breadcrumbs from "./components/Breadcrumbs"
+import Cards from "./components/Cards"
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Error404/>
       <Pagination/>
       <Tabs/>
+      <Breadcrumbs/>
+      <Cards/>
       <h1 className="text-4xl text-green-500 font-bold text-center mt-10">
         Employee Management System
       </h1>
