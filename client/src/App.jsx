d5947fadf6ee26adffc203099d23b5aa72806750
Charlize Mikaela Nadela <105686280+charlysss-l@users.noreply.github.com>
2026-05-15 10:11:17 +0000

    header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,12 @@
+import Header from './components/Header'
 function App() {
   return (
-    <h1 className="text-4xl text-green-500 font-bold text-center mt-10">
-      Employee Management System
-    </h1>
+    <div className="bg-gray-100 min-h-screen">
+      <Header/>
+      <h1 className="text-4xl text-green-500 font-bold text-center mt-10">
+        Employee Management System
+      </h1>
+    </div>
   )
 }
 
