6210a9805fffb377b15b80b8b9ec849eee5db754
Charlize Mikaela Nadela <105686280+charlysss-l@users.noreply.github.com>
2026-05-15 10:22:12 +0000

    components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,15 @@
 import Header from './components/Header'
+import Error404 from './components/Error404'
+import Pagination from "./components/Paginaton"
+import Tabs from "./components/Tabs"
+
 function App() {
   return (
     <div className="bg-gray-100 min-h-screen">
       <Header/>
+      <Error404/>
+      <Pagination/>
+      <Tabs/>
       <h1 className="text-4xl text-green-500 font-bold text-center mt-10">
         Employee Management System
       </h1>
